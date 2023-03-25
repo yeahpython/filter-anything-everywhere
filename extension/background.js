f6c0519f03b76e19442601c85f44ed375216a797
Tommy Li <123tom.li@gmail.com>
2023-03-24 19:49:46 -0700

    Use camel case in background.js

--- extension/background.js
+++ extension/background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-  const count_string = msg.count ? msg.count.toString() : '';
+  const countString = msg.count ? msg.count.toString() : '';
   chrome.action.setBadgeBackgroundColor({color: [100, 100, 100, 255]});
-  chrome.action.setBadgeText({text: count_string, tabId: sender.tab.id});
+  chrome.action.setBadgeText({text: countString, tabId: sender.tab.id});
 });
 
 chrome.runtime.setUninstallURL('https://goo.gl/forms/uNxb8D4S8B9YeEln2');
